f28ace4a2e6ff98303a7b9d0d54a0a535bc64b6c
Archit Sharma <archit8679@gmail.com>
2024-07-16 23:26:49 +0530

    fix
    Signed-off-by: Archit Sharma <archit8679@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: '/webcosmos.in', 
+    assetPrefix: '/webcosmos.in/',
     output: 'export',
 };
 
