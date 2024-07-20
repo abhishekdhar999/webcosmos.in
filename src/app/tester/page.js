024420f0940d448233e469e892738ff735e44f0a
Archit Sharma <archit8679@gmail.com>
2024-07-20 18:20:36 +0530

    Added Articles section
    Signed-off-by: Archit Sharma <archit8679@gmail.com>

--- src/app/tester/page.js
+++ src/app/tester/page.js
@@ -3,12 +3,14 @@ import FeatureSection from '../components/Feature/FeatureSection';
 import TestimonialSection from '../components/Testimonials/TestimonialsSection';
 import { Subscribe } from '../components/Footer/subscribe';
 import { Footer } from '../components/Footer/footer';
+import { BlogSection } from '../components/Blogs/BlogSection';
 
 const About = () => {
   return (
     <div>
          <FeatureSection />
          <TestimonialSection/>
+         <BlogSection />
          <Subscribe/>
          <Footer/>
     </div>
