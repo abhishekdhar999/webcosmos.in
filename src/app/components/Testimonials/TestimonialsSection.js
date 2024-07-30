c5d41d945cce5770c7533af3e82e75cc8542a36f
Abhishek Dhar <dhar.abhishek999@gmail.com>
2024-07-30 17:31:51 +0530

    final
    Signed-off-by: Abhishek Dhar <dhar.abhishek999@gmail.com>

--- src/app/components/Testimonials/TestimonialsSection.js
+++ src/app/components/Testimonials/TestimonialsSection.js
@@ -24,38 +24,33 @@ const data = [
     testimonial: "Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application.",
   },
 ];
-import { BsBoxArrowInUpRight } from "react-icons/bs";
-import { FaMicrophoneAlt } from "react-icons/fa";
 
 const TestimonialSection = () => {
   return (
-    <div className="bg-gray-900 py-12 px-4 sm:px-8 lg:px-24">
-      {/* Responsive container for the heading */}
-      <div className="services-heading flex flex-col md:flex-row justify-between items-center md:items-start mx-auto lg:mx-36 lg:my-24 my-16">
-        {/* Left side with the heading */}
-        <div className="left text-center md:text-left">
-          <h1 className="head-1 text-white my-0 text-5xl md:text-[clamp(3.125rem,15vw,10rem)] leading-tight">
-            Users
-          </h1>
-          <h1 className="head-1 text-white md:ml-36 text-5xl md:text-[clamp(3.125rem,15vw,10rem)] leading-tight">
-            Voice
-          </h1>
+    <div>
+    <div class="-mx-4 flex flex-wrap justify-center">
+          <div class="w-full px-4">
+            <div class="mx-auto mb-[60px] max-w-[485px] text-center">
+              <span class="mb-2 block text-lg font-semibold text-primary">
+                Testimonials
+              </span>
+              <h2
+                class="mb-3 text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-4xl md:text-[40px]"
+              >
+                What our Clients Say
+              </h2>
+              <p class="text-base text-body-color dark:text-dark-6">
+                There are many variations of passages of Lorem Ipsum available
+                but the majority have suffered alteration in some form.
+              </p>
+            </div>
+          </div>
         </div>
         <Testimonials testimonials={data} showViewMore="true"/>
         </div>
-
-        {/* Right side with the icon */}
-        <div className="right mt-6 md:mt-36 flex justify-center md:justify-start">
-          <span className="text-7xl sm:text-8xl lg:text-9xl text-blue-500">
-            <FaMicrophoneAlt />
-          </span>
-        </div>
-      </div>
-      
-      {/* Testimonials component */}
-      <Testimonials />
-    </div>
   );
 };
+
+
 
 export default TestimonialSection;
